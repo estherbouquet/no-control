dd026b4f6c9c77af5c703b2ca33148e60eae7d59
Esther Bouquet <ebouquet@orange.fr>
2019-05-30 16:48:30 +0200

    Create potentiometer.ino

---
+++ potentiometer/potentiometer.ino
@@ -0,0 +1,14 @@
+int pot_pin = A0;   // Initializing the Potentiometer pin
+int pot_output; // Declaring a variable for potentiometer output
+
+void setup() {
+  Serial.begin(9600);       // Starting the serial communication at 9600 baud rate
+}
+
+void loop() {
+  pot_output = analogRead (pot_pin); // Reading from the potentiometer
+  int mapped_output = map (pot_output, 0, 1023, 0, 20); // Mapping the output of potentiometer 
+  Serial.println (mapped_output);     // Sending the output to Processing IDE
+  delay(50);
+
+}
